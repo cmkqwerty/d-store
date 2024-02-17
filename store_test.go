eedb3c44ede672a9843a18341c4b5862fae278e7
cmkqwerty <56502922+cmkqwerty@users.noreply.github.com>
2024-02-18 00:09:06 +0300

    [feat] finish cas

--- store_test.go
+++ store_test.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"testing"
 )
 
 func TestPathTransformFunc(t *testing.T) {
 	key := "my-special-picture"
-	expectedOriginalKey := "ea842a6587df2f0961463b74cde7959393acd483"
+	expectedFilename := "ea842a6587df2f0961463b74cde7959393acd483"
 	expectedPathName := "ea842/a6587/df2f0/96146/3b74c/de795/9393a/cd483"
 	actual := CASPathTransformFunc(key)
 
 	if actual.PathName != expectedPathName {
 		t.Fatalf("expected %s, got %s", expectedPathName, actual.PathName)
 	}
-	if actual.Filename != expectedOriginalKey {
-		t.Fatalf("expected %s, got %s", expectedOriginalKey, actual.Filename)
+	if actual.Filename != expectedFilename {
+		t.Fatalf("expected %s, got %s", expectedFilename, actual.Filename)
 	}
 }
 
@@ -38,36 +39,56 @@ func TestStoreDeleteKey(t *testing.T) {
 }
 
 func TestStore(t *testing.T) {
+	s := newStore()
+	defer teardown(t, s)
+
+	for i := 0; i < 50; i++ {
+		key := fmt.Sprintf("secret_%d", i)
+		data := []byte("hello, world")
+
+		if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+			t.Fatal(err)
+		}
+
+		if ok := s.Has(key); !ok {
+			t.Fatalf("expected %s to exist", key)
+		}
+
+		r, err := s.Read(key)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		b, err := io.ReadAll(r)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if string(b) != string(data) {
+			t.Fatalf("expected %s, got %s", data, b)
+		}
+
+		if err := s.Delete(key); err != nil {
+			t.Fatal(err)
+		}
+
+		if ok := s.Has(key); ok {
+			t.Fatalf("expected %s to NOT exist", key)
+		}
+	}
+}
+
+func newStore() *Store {
 	opts := StoreOpts{
+		Root:              "",
 		PathTransformFunc: CASPathTransformFunc,
 	}
-	s := NewStore(opts)
-	key := "my-special-picture"
-	data := []byte("hello, world")
 
-	if err := s.writeStream("my-special-picture", bytes.NewReader(data)); err != nil {
-		t.Fatal(err)
-	}
+	return NewStore(opts)
+}
 
-	if ok := s.Has(key); !ok {
-		t.Fatalf("expected %s to exist", key)
-	}
-
-	r, err := s.Read(key)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	b, err := io.ReadAll(r)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if string(b) != string(data) {
-		t.Fatalf("expected %s, got %s", data, b)
-	}
-
-	if err := s.Delete(key); err != nil {
+func teardown(t *testing.T, s *Store) {
+	if err := s.Clear(); err != nil {
 		t.Fatal(err)
 	}
 }
